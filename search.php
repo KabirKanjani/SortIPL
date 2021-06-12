29e47629828fb1d96d98db4818ff4cb98b507780
unknown <kavitakanjani24@gmail.com>
2021-06-12 22:03:09 +0530

    Added

--- search.php
+++ search.php
@@ -1,6 +1,15 @@
 <?php
 
-$connect=mysqli_connect("localhost:3308","root","","ipl");
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+$cleardb_username = $cleardb_url["b5a264c5c9d4c6"];
+$cleardb_password = $cleardb_url["b8abe23f"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+    $connect=mysqli_connect($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
+
 if(isset($_POST['query']))
 {
   $output='';
